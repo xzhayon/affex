22046151917e4644a2dd683c03232118acb98a9e
Alberto Villa <alberto.villa@facile.it>
2024-04-23 07:59:49 +0000

    feat: export test-oriented methods

--- src/exports.ts
+++ src/exports.ts
@@ -1,7 +1,10 @@
-export { isDie, isFail } from './Cause'
+import { die, fail } from './Cause'
+import { failure, success } from './Exit'
+
+export { is as isCause, isDie, isFail } from './Cause'
 export { AsyncEffector, Effector } from './Effector'
 export { tryCatch } from './Error'
-export { isFailure, isSuccess } from './Exit'
+export { is as isExit, isFailure, isSuccess } from './Exit'
 export { NOf, ROf, TOf, UOf, YOf, sequence, traverse } from './Generator'
 export { layer } from './Layer'
 export { all, any, race, settled } from './Promise'
@@ -12,3 +15,5 @@ export { uri } from './Type'
 export { raise } from './effect/Exception'
 export { fork } from './effect/Fork'
 export { function, functionA, struct, structA } from './effect/Proxy'
+export const Cause = { die, fail }
+export const Exit = { failure, success }
