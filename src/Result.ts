46dab2fe9567e8148536193cecd6eb8b4278aaa0
Alberto Villa <alberto.villa@facile.it>
2024-04-23 08:35:46 +0000

    style: sort code

--- src/Result.ts
+++ src/Result.ts
@@ -12,8 +12,6 @@ interface Failure<E> extends _Result<'Failure'> {
   readonly error: E
 }
 
-declare const uri: unique symbol
-
 export type AOf<R extends Result<any, any>> = R extends Success<infer A>
   ? A
   : never
@@ -22,3 +20,5 @@ export type EOf<R extends Result<any, any>> = R extends Failure<infer E>
   : never
 
 export type Resulted<A> = A extends Result<any, any> ? AOf<A> : A
+
+declare const uri: unique symbol
