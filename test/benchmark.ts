dbda0799b52bf028335ed65cf318132640f62c03
Alberto Villa <alberto.villa@facile.it>
2024-05-09 08:00:52 +0000

    perf: make benchmark measure asynchronous tasks

--- test/benchmark.ts
+++ test/benchmark.ts
@@ -1,154 +1,112 @@
 import { Effect } from 'effect'
 import * as effection from 'effection'
 import { Bench } from 'tinybench'
-import { AsyncEffector } from '../src/Effector'
 import * as $Layer from '../src/Layer'
 import * as $Promise from '../src/Promise'
 import * as $Runtime from '../src/Runtime'
+import * as $Tag from '../src/Tag'
+import { uri } from '../src/Type'
+import * as $Proxy from '../src/effect/Proxy'
 
 async function benchmark() {
+  interface Sleep {
+    readonly [uri]?: unique symbol
+    (ms: number): Promise<void>
+  }
+
   function sleep(ms: number) {
-    return new Promise((resolve) => setTimeout(resolve, ms))
+    return new Promise<void>((resolve) => setTimeout(resolve, ms))
   }
 
-  async function vanillaSeq(n: number): Promise<number> {
-    await sleep(0)
-    switch (n) {
-      case 0:
-      case 1:
-        return n
-      default:
-        return (await vanillaSeq(n - 2)) + (await vanillaSeq(n - 1))
+  const tag = $Tag.tag<Sleep>()
+  const affexSleep = $Proxy.function(tag)
+  const layer = $Layer.layer().with(tag, sleep)
+  const runtime = $Runtime.runtime(layer)
+
+  async function vanillaSeq(n: number, ms: number) {
+    for (let i = 0; i < n; i++) {
+      await sleep(ms)
     }
   }
 
-  async function vanillaPar(n: number): Promise<number> {
-    await sleep(0)
-    switch (n) {
-      case 0:
-      case 1:
-        return n
-      default:
-        const [a, b] = await Promise.all([vanillaPar(n - 2), vanillaPar(n - 1)])
-
-        return a + b
-    }
+  async function vanillaPar(n: number, ms: number) {
+    await Promise.all(
+      Array(n)
+        .fill(undefined)
+        .map(() => sleep(ms)),
+    )
   }
 
-  function effectSeq(n: number): Effect.Effect<number> {
+  function effectSeq(n: number, ms: number) {
     return Effect.gen(function* () {
-      yield* Effect.sleep(0)
-      switch (n) {
-        case 0:
-        case 1:
-          return n
-        default:
-          return (yield* effectSeq(n - 2)) + (yield* effectSeq(n - 1))
-      }
+      yield* Effect.forEach(Array(n), () => Effect.sleep(ms), {
+        concurrency: 0,
+      })
     })
   }
 
-  function effectPar(n: number): Effect.Effect<number> {
+  function effectPar(n: number, ms: number) {
     return Effect.gen(function* () {
-      yield* Effect.sleep(0)
-      switch (n) {
-        case 0:
-        case 1:
-          return n
-        default:
-          const [a, b] = yield* Effect.all(
-            [effectPar(n - 2), effectPar(n - 1)],
-            { concurrency: 'unbounded' },
-          )
-
-          return a + b
-      }
+      yield* Effect.forEach(Array(n), () => Effect.sleep(ms), {
+        concurrency: 'unbounded',
+      })
     })
   }
 
-  function* effectionSeq(n: number): effection.Operation<number> {
-    yield* effection.sleep(0)
-    switch (n) {
-      case 0:
-      case 1:
-        return n
-      default:
-        return (yield* effectionSeq(n - 2)) + (yield* effectionSeq(n - 1))
+  function* effectionSeq(n: number, ms: number) {
+    for (let i = 0; i < n; i++) {
+      yield* effection.sleep(ms)
     }
   }
 
-  function* effectionPar(n: number): effection.Operation<number> {
-    yield* effection.sleep(0)
-    switch (n) {
-      case 0:
-      case 1:
-        return n
-      default:
-        const [a, b] = yield* effection.all([
-          effectionPar(n - 2),
-          effectionPar(n - 1),
-        ])
+  function* effectionPar(n: number, ms: number) {
+    yield* effection.all(
+      Array(n)
+        .fill(undefined)
+        .map(() => effection.sleep(ms)),
+    )
+  }
 
-        return a + b
+  function* affexSeq(n: number, ms: number) {
+    for (let i = 0; i < n; i++) {
+      yield* affexSleep(ms)
     }
   }
 
-  async function* affexSeq(n: number): AsyncEffector<number, never, never> {
-    await sleep(0)
-    switch (n) {
-      case 0:
-      case 1:
-        return n
-      default:
-        return (yield* affexSeq(n - 2)) + (yield* affexSeq(n - 1))
-    }
+  function* affexParPromise(n: number, ms: number) {
+    yield* $Promise.all(
+      Array(n)
+        .fill(undefined)
+        .map(() => affexSleep(ms)),
+    )
   }
-
-  async function* affexParPromise(
-    n: number,
-  ): AsyncEffector<number, never, never> {
-    await sleep(0)
-    switch (n) {
-      case 0:
-      case 1:
-        return n
-      default:
-        const [a, b] = yield* $Promise.all([
-          affexParPromise(n - 2),
-          affexParPromise(n - 1),
-        ])
-
-        return a + b
-    }
-  }
-
-  const runtime = $Runtime.runtime($Layer.layer())
 
   const n = 10
+  const ms = 10
   const bench = new Bench()
     .add('vanilla: sequential', async () => {
-      await vanillaSeq(n)
+      await vanillaSeq(n, ms)
     })
     .add('vanilla: parallel', async () => {
-      await vanillaPar(n)
+      await vanillaPar(n, ms)
     })
     .add('effect: sequential', async () => {
-      await Effect.runPromise(effectSeq(n))
+      await Effect.runPromise(effectSeq(n, ms))
     })
     .add('effect: parallel', async () => {
-      await Effect.runPromise(effectPar(n))
+      await Effect.runPromise(effectPar(n, ms))
     })
     .add('effection: sequential', async () => {
-      await effection.run(() => effectionSeq(n))
+      await effection.run(() => effectionSeq(n, ms))
     })
     .add('effection: parallel', async () => {
-      await effection.run(() => effectionPar(n))
+      await effection.run(() => effectionPar(n, ms))
     })
     .add('affex: sequential', async () => {
-      await runtime.run(affexSeq(n))
+      await runtime.run(affexSeq(n, ms))
     })
     .add('affex: parallel', async () => {
-      await runtime.run(affexParPromise(n))
+      await runtime.run(affexParPromise(n, ms))
     })
 
   await bench.warmup()
