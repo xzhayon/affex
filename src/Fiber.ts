158a4a15edbed10bb10ceb5238320bd42cad080c
Alberto Villa <alberto.villa@facile.it>
2024-05-26 21:49:46 +0000

    perf: speedup interruption of concurrent fibers

--- src/Fiber.ts
+++ src/Fiber.ts
@@ -5,17 +5,16 @@ import * as $Type from './Type'
 import { OrLazy } from './Type'
 import * as $Fork from './effect/Fork'
 import * as $Join from './effect/Join'
-import * as $Scope from './effect/Scope'
 
-export function all<G extends AnyEffector<any, any, any>>(
+export function* all<G extends AnyEffector<any, any, any>>(
   effectors: ReadonlyArray<OrLazy<G>>,
-) {
-  return $Scope.scope(function* (): Effector<
-    ReturnOf<G>[],
-    YieldOf<G> extends infer Y ? (Y extends Throw<infer E> ? E : never) : never,
-    YieldOf<G> extends infer Y ? (Y extends Use<infer R> ? R : never) : never
-  > {
-    const fibers = yield* $Generator.traverse(effectors, $Fork.fork)
+): Effector<
+  ReturnOf<G>[],
+  YieldOf<G> extends infer Y ? (Y extends Throw<infer E> ? E : never) : never,
+  YieldOf<G> extends infer Y ? (Y extends Use<infer R> ? R : never) : never
+> {
+  const fibers = yield* $Generator.traverse(effectors, $Fork.fork)
+  try {
     const values = []
     let done = 0
     for (let i = 0; done < fibers.length; i++) {
@@ -36,25 +35,29 @@ export function all<G extends AnyEffector<any, any, any>>(
 
           break
         default:
-          yield undefined as never
+          yield
 
           break
       }
     }
 
     return values
-  })
+  } finally {
+    yield* $Generator.fromPromise(
+      Promise.all(fibers.map((fiber) => fiber.interrupt())),
+    )
+  }
 }
 
-export function any<G extends AnyEffector<any, any, any>>(
+export function* any<G extends AnyEffector<any, any, any>>(
   effectors: ReadonlyArray<OrLazy<G>>,
-) {
-  return $Scope.scope(function* (): Effector<
-    ReturnOf<G>,
-    never,
-    YieldOf<G> extends infer Y ? (Y extends Use<infer R> ? R : never) : never
-  > {
-    const fibers = yield* $Generator.traverse(effectors, $Fork.fork)
+): Effector<
+  ReturnOf<G>,
+  never,
+  YieldOf<G> extends infer Y ? (Y extends Use<infer R> ? R : never) : never
+> {
+  const fibers = yield* $Generator.traverse(effectors, $Fork.fork)
+  try {
     const errors = []
     let done = 0
     for (let i = 0; done < fibers.length; i++) {
@@ -80,25 +83,29 @@ export function any<G extends AnyEffector<any, any, any>>(
         case 'Terminated':
           return fiber.status.value
         default:
-          yield undefined as never
+          yield
 
           break
       }
     }
 
     throw new AggregateError(errors, 'All fibers failed')
-  })
+  } finally {
+    yield* $Generator.fromPromise(
+      Promise.all(fibers.map((fiber) => fiber.interrupt())),
+    )
+  }
 }
 
-export function race<G extends AnyEffector<any, any, any>>(
+export function* race<G extends AnyEffector<any, any, any>>(
   effectors: ReadonlyArray<OrLazy<G>>,
-) {
-  return $Scope.scope(function* (): Effector<
-    ReturnOf<G>,
-    YieldOf<G> extends infer Y ? (Y extends Throw<infer E> ? E : never) : never,
-    YieldOf<G> extends infer Y ? (Y extends Use<infer R> ? R : never) : never
-  > {
-    const fibers = yield* $Generator.traverse(effectors, $Fork.fork)
+): Effector<
+  ReturnOf<G>,
+  YieldOf<G> extends infer Y ? (Y extends Throw<infer E> ? E : never) : never,
+  YieldOf<G> extends infer Y ? (Y extends Use<infer R> ? R : never) : never
+> {
+  const fibers = yield* $Generator.traverse(effectors, $Fork.fork)
+  try {
     for (let i = 0; true; i++) {
       const fiber = fibers[i % fibers.length]
       switch (fiber.status[$Type.tag]) {
@@ -108,10 +115,14 @@ export function race<G extends AnyEffector<any, any, any>>(
         case 'Terminated':
           return fiber.status.value
         default:
-          yield undefined as never
+          yield
 
           break
       }
     }
-  })
+  } finally {
+    yield* $Generator.fromPromise(
+      Promise.all(fibers.map((fiber) => fiber.interrupt())),
+    )
+  }
 }
